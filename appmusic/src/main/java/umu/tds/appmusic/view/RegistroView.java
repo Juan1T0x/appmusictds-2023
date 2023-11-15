9ff86d6a18a525266d8940f9fcc55fd460d55340
Juan1T0x <mazarronjuan12@gmail.com>
2023-11-15 17:33:49 +0100

    Creadas nuevas clases
    Se han creado las clases de login y registro

---
+++ appmusic/src/main/java/umu/tds/appmusic/view/RegistroView.java
@@ -0,0 +1,105 @@
+package umu.tds.appmusic.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.BoxLayout;
+import javax.swing.border.TitledBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class RegistroView extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textUser;
+	private JTextField textPassword;
+	private JTextField textEmail;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RegistroView frame = new RegistroView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RegistroView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelNorte = new JPanel();
+		contentPane.add(panelNorte, BorderLayout.NORTH);
+		
+		JLabel lblTitulo = new JLabel("AppMusic");
+		lblTitulo.setFont(new Font("Apple Braille", Font.PLAIN, 21));
+		panelNorte.add(lblTitulo);
+		
+		JPanel panelCentral = new JPanel();
+		contentPane.add(panelCentral, BorderLayout.CENTER);
+		panelCentral.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelCampos = new JPanel();
+		panelCentral.add(panelCampos, BorderLayout.CENTER);
+		panelCampos.setLayout(new BoxLayout(panelCampos, BoxLayout.Y_AXIS));
+		
+		JPanel panelUser = new JPanel();
+		panelUser.setBorder(new TitledBorder(null, "User", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCampos.add(panelUser);
+		panelUser.setLayout(new BorderLayout(0, 0));
+		
+		textUser = new JTextField();
+		panelUser.add(textUser, BorderLayout.CENTER);
+		textUser.setColumns(10);
+		
+		JPanel panelPassword = new JPanel();
+		panelPassword.setBorder(new TitledBorder(null, "Password", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCampos.add(panelPassword);
+		panelPassword.setLayout(new BorderLayout(0, 0));
+		
+		textPassword = new JTextField();
+		panelPassword.add(textPassword, BorderLayout.CENTER);
+		textPassword.setColumns(10);
+		
+		JPanel panelEmail = new JPanel();
+		panelEmail.setBorder(new TitledBorder(null, "Email", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCampos.add(panelEmail);
+		panelEmail.setLayout(new BorderLayout(0, 0));
+		
+		textEmail = new JTextField();
+		panelEmail.add(textEmail, BorderLayout.CENTER);
+		textEmail.setColumns(10);
+		
+		JPanel panelBirth = new JPanel();
+		panelBirth.setBorder(new TitledBorder(null, "Birth Date", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCampos.add(panelBirth);
+		panelBirth.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelBotones = new JPanel();
+		panelCentral.add(panelBotones, BorderLayout.SOUTH);
+		
+		JButton btnRegister = new JButton("Register");
+		panelBotones.add(btnRegister);
+	}
+
+}
