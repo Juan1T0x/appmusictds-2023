ad51556081f8a5939e510f9e20656738a665ab8a
Salvador Candel <salvacandel@gmail.com>
2023-12-12 18:20:17 +0100

    Funciona LoginView y RegistroView

--- appmusic/src/main/java/umu/tds/view/RegistroView.java
+++ appmusic/src/main/java/umu/tds/view/RegistroView.java
@@ -1,17 +1,17 @@
-package umu.tds.appmusic.view;
+package umu.tds.view;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
-import javax.swing.JLabel;
+import java.awt.EventQueue;
 import java.awt.Font;
+
 import javax.swing.BoxLayout;
-import javax.swing.border.TitledBorder;
-import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 public class RegistroView extends JFrame {
 
